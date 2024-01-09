c12c4b54f13cbbab4af7affe8d14f18f31a1a3c0
sanjave <ssingh025@citymail.cuny.edu>
2024-01-08 20:26:05 -0400

    Day 37: Threading in Python

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import glob
 import cv2
 import time
+import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
+def clean_folder():
+    print("Clean_folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("clean_floder function ended")
 while True:
     status = 0
     check, frame = video.read()
@@ -32,12 +42,24 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email,args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+
+    print(status_list)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
@@ -46,6 +68,8 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
 
 
 
